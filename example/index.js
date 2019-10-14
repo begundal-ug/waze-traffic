6c18910199ab730342093f1f5f19e0758b44db1a
Ahmad Anshorimuslim Syuhada <ans4175@gmail.com>
2019-10-14 07:55:10 +0700

    Feat/speed kmh (#16)
    * fix(api): change API to community token
    
    * feat(jams): add speedKMH
    
    * FIX: indentation on example
    FIX: remove npm locks
    FEAT: update yarn lock

--- example/index.js
+++ example/index.js
@@ -1,10 +1,10 @@
 const waze = require('../')
 
 waze.getTraffic({
-  top: '-6.875856',
-  bottom: '-6.950494',
-  left: '107.564822',
-  right: '107.674711'
+  top: '-6.89206',
+  right: '107.64529',
+  bottom: '-6.89883',
+  left: '107.63186'
 }).then(result => {
   console.log('==============ALERTS================')
   result.alerts.forEach(alert => console.log(alert))
